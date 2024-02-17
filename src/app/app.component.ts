8a25a57367d230c7f6bce0fb25827f0398727bd3
Sebastian Leon <jsleono@ufpso.edu.co>
2024-02-17 18:50:44 -0500

    FINAL

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,17 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'actividad3';
+
+  audioPlaying: boolean = false;
+
+  reproduceAudio1() {
+    this.audioPlaying = true;
+    const audio = new Audio();
+    audio.src = './assets/sounds/Audio-test-1.mp3';
+    audio.load();
+    audio.play();
+    audio.onended = () => {
+      this.audioPlaying = false;
+    };
+  }
 }
